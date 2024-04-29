a56728a775b4999d7e827f5f37f1954dd144ba53
Hayden Lister <haydenglister@gmail.com>
2024-04-29 15:15:14 -0700

    Added tests for getURLsFromHTML function

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,7 @@
 import { test, expect } from "@jest/globals"
-import { normalizeURL } from "./crawl.js"
+import { normalizeURL, getURLsFromHTML } from "./crawl.js"
+
+// normalizeURL tests
 
 test("normalizeURL base test", () => {
     const input = "https://example.com/path"
@@ -47,5 +49,37 @@ test("normalizeURL query test", () => {
     const input = "https://example.com/path?query=this"
     const output = normalizeURL(input)
     const expected = "example.com/path"
+    expect(output).toEqual(expected)
+})
+
+// getURLsFromHTML tests
+
+test("getURLsFromHTML absolute path test", () => {
+    const input = "<html><body><div><a href='https://blog.example.com'>Absolute link</a> </div></html></body>"
+    const baseUrl = "https://example.com"
+    const output = getURLsFromHTML(input, baseUrl)
+    const expected = ['https://blog.example.com/']
+    console.log(output)
+    console.log(expected)
+    expect(output).toEqual(expected)
+})
+
+test("getURLsFromHTML relative path test", () => {
+    const input = "<html><body><div><a href='/newpath'>Relative link</a></div></html></body>"
+    const baseUrl = "https://example.com"
+    const output = getURLsFromHTML(input, baseUrl)
+    const expected = ['https://example.com/newpath']
+    console.log(output)
+    console.log(expected)
+    expect(output).toEqual(expected)
+})
+
+test("getURLsFromHTML absolute and relative path test", () => {
+    const input = "<html><body><div><a href='/newpath/anotherpath'>Relative Link</a><a href='https://other.com/path'>Absolute Link</a></div></html></body>"
+    const baseUrl = "https://example.com"
+    const output = getURLsFromHTML(input, baseUrl)
+    const expected = ['https://example.com/newpath/anotherpath', 'https://other.com/path']
+    console.log(output)
+    console.log(expected)
     expect(output).toEqual(expected)
 })
