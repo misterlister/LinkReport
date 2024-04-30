378a76a6865ba439bfa9b0f15c6ec8eb683705a2
Hayden Lister <haydenglister@gmail.com>
2024-04-29 18:28:08 -0700

    Added argv checking and calling of crawlPage function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+import { crawlPage } from "./crawl.js"
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log("Error: No website provided")
+        return
+    }
+    if (process.argv.length > 3) {
+        console.log("Error: Too many arguments")
+        return
+    }
+    const baseURL = process.argv[2]
+    console.log(`Beginning scan of: ${baseURL}...`)
+    const pages = await crawlPage(baseURL)
+    console.log(pages)
+}
+
+main()
